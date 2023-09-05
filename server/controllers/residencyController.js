691c48759d45c4658a7ee26f9608f1b610a2a657
Chandradeep Chandra <cchandra554@gmail.com>
2023-09-05 11:48:45 +0530

    create user, create residency route completed
    Signed-off-by: Chandradeep Chandra <cchandra554@gmail.com>

---
+++ server/controllers/residencyController.js
@@ -0,0 +1,38 @@
+import asyncHandler from "express-async-handler";
+import { prisma } from "../config/prismaConfig.js";
+
+export const createResidency = asyncHandler(async (req, res) => {
+  const {
+    title,
+    description,
+    price,
+    address,
+    country,
+    city,
+    facilities,
+    image,
+    userEmail,
+  } = req.body.data;
+
+  try {
+    const residency = await prisma.residency.create({
+      data: {
+        title,
+        description,
+        price,
+        address,
+        country,
+        city,
+        facilities,
+        image,
+        owner: { connect: { email: userEmail } },
+      },
+    });
+    res.send({ message: "Residency created successfully", residency });
+  } catch (err) {
+    if (err.code === "P2002") {
+      throw new Error("A residency with address is already there");
+    }
+    throw new Error(err.message);
+  }
+});
