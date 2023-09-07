ef20fef826aba874b68c2cd96f61893d9203d459
Chandradeep Chandra <cchandra554@gmail.com>
2023-09-07 11:09:42 +0530

    Completed residency api
    Signed-off-by: Chandradeep Chandra <cchandra554@gmail.com>

--- server/controllers/residencyController.js
+++ server/controllers/residencyController.js
@@ -36,3 +36,27 @@ export const createResidency = asyncHandler(async (req, res) => {
     throw new Error(err.message);
   }
 });
+
+// get all the residencies
+export const getAllResidencies = asyncHandler(async (req, res) => {
+  const residencies = await prisma.residency.findMany({
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+  res.send(residencies);
+});
+
+// get a specific residency
+export const getResidency = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const residency = await prisma.residency.findUnique({
+      where: { id },
+    });
+    res.send(residency);
+  } catch (err) {
+    throw new Error(err.message);
+  }
+});
--- server/routes/residencyRoute.js
+++ server/routes/residencyRoute.js
@@ -1,7 +1,9 @@
 import express from 'express'
-import { createResidency } from '../controllers/residencyController.js'
+import { createResidency, getAllResidencies, getResidency } from '../controllers/residencyController.js'
 const router = express.Router()
 
 router.post('/create', createResidency)
+router.get('/allresd', getAllResidencies)
+router.get('/:id', getResidency)
 
 export {router as residencyRoute}
