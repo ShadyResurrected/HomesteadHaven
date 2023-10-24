739142f2f75faf2d1822c4f5969fc77018d9bbb1
ShadyResurrected <cchandra554@gmail.com>
2023-10-24 12:17:00 +0530

    completed slider/residencies
    Signed-off-by: ShadyResurrected <cchandra554@gmail.com>

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,8 @@ import Header from "./components/Header/Header";
 import Hero from "./components/Hero/Hero";
 
 import './App.css'
+import Companies from "./components/Companies/Companies";
+import Residencies from "./components/Residencies/Residencies";
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
         <Header />
         <Hero />
       </div>
+      <Companies/>
+      <Residencies/>
     </div>
   );
 }
